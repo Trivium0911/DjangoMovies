d482925187b38573ad3bcef6312d6ae15b350509
Trivium0911 <triv_1993@mail.ru>
2023-05-12 09:48:18 +0300

    added slug instead of int in MovieDetailView

--- django_movies/movies/urls.py
+++ django_movies/movies/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.MovieView.as_view()),
-    path("<int:pk>/", views.MovieDetailView.as_view())
+    path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail")
 
 ]
